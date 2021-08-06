1efb7a7f77e30d8e8973d5294ed14565c935fe5a
rgupta1 <rgupta1@CL91120P5.westrock.com>
2021-08-06 06:12:03 -0400

    v1

---
+++ assets/IS/Packages/Dev_rajeev/code/source/Dev_rajeev/ServiceOne.java
@@ -0,0 +1,50 @@
+package Dev_rajeev;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class ServiceOne
+
+{
+	// ---( internal utility methods )---
+
+	final static ServiceOne _instance = new ServiceOne();
+
+	static ServiceOne _newInstance() { return new ServiceOne(); }
+
+	static ServiceOne _cast(Object o) { return (ServiceOne)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void getHostName (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(getHostName)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void new_javaService (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(new_javaService)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
